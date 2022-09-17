501a802331401917220ed5174af47036759c0ecf
Dagan Henderson <dagan@techdagan.com>
2022-09-17 16:44:36 -0700

    Updates dependencies

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@
 */
 
 // Package v1alpha1 contains API Schema definitions for the konfirm v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=konfirm.goraft.tech
+// +kubebuilder:object:generate=true
+// +groupName=konfirm.goraft.tech
 package v1alpha1
 
 import (
